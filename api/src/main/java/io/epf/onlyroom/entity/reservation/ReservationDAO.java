9032ae98d781820dd38f9eea35da7d6f0a9b4a01
55750061 <delattre.hugo@gmail.com>
2020-11-01 19:29:15 +0100

    fix bugs

--- api/src/main/java/io/epf/onlyroom/entity/reservation/ReservationDAO.java
+++ api/src/main/java/io/epf/onlyroom/entity/reservation/ReservationDAO.java
@@ -13,7 +13,7 @@ import java.util.List;
  */
 @Repository
 public interface ReservationDAO extends CrudRepository<Reservation, Long> {
-    @Query("SELECT resa FROM reservations resa WHERE (resa.date = ?1 AND resa.room.id = ?4 AND ((resa.beginHour >= ?2 AND resa.beginHour <= ?3) OR (resa.endHour >= ?2 AND resa.endHour <= ?3)))")
+    @Query("SELECT resa FROM reservations resa WHERE (resa.date = ?1 AND resa.room.id = ?4 AND ((resa.beginHour <= ?2 AND resa.endHour >= ?2) OR (resa.beginHour <= ?3 AND resa.endHour >= ?3)))")
     List<Reservation> currentReservations(Date date, Integer beginHour, Integer endHour, Long roomId);
 
     List<Reservation> findByUserId(Long userId);
